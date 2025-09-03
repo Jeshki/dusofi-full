3bad0cb2d39ecfdd45378b82bcdca85e4e344421
Karolis Cibiras <83600971+Jeshki@users.noreply.github.com>
2025-09-03 21:58:53 +0300

    feat: extend seo with manifest and crawler directives

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -238,9 +238,7 @@ export default function SEO({
       
       {/* Favicons */}
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="apple-touch-icon" sizes="512x512" href="/images/dusofi-logo.png" />
       <link rel="manifest" href="/site.webmanifest" />
       
       {/* DNS Prefetch for performance */}
